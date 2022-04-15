9364bfa353a8cf8a2299561bd16fafa1e23a7f5c
Night <davidjantho@gmail.com>
2022-04-16 00:58:04 +0200

    f

--- install.py
+++ install.py
@@ -5,7 +5,7 @@ import subprocess
 try:
     with open("keep-server-awake.service.template", 'r') as f:
         service = f.read(-1)
-    service.replace("<file_path>", f'{getcwdb().decode("utf-8")}/main.py')
+    service = service.replace("<file_path>", f'{getcwdb().decode("utf-8")}/main.py')
     with open("/etc/systemd/system/keep-server-awake.service", "w") as f:
         f.write(service)
     subprocess.call(["sudo", "systemctl", "daemon-reload"])
