94633be1693ad7188417e4020e07cd72a7f3f456
Night <davidjantho@gmail.com>
2022-04-16 00:23:23 +0200

    Fix

--- install.py
+++ install.py
@@ -8,8 +8,8 @@ try:
     service.replace(r"{file_path}", f'{getcwdb().decode("utf-8")}/main.py')
     with open("/etc/systemd/system/keep-server-awake.service", "w") as f:
         f.write(service)
-    subprocess.call(["sudo", "systemctle", "daemon-reload"])
-    subprocess.call(["sudo", "systemctle", "enable", "keep-server-awake.service"])
-    subprocess.call(["sudo", "systemctle", "start", "keep-server-awake.service"])
+    subprocess.call(["sudo", "systemctl", "daemon-reload"])
+    subprocess.call(["sudo", "systemctl", "enable", "keep-server-awake.service"])
+    subprocess.call(["sudo", "systemctl", "start", "keep-server-awake.service"])
 except Exception as ex:
     print("Service creation failed, please try starting this with sudo!")
