f3ad65a0cc887c21af40f51545c6a765031edd3e
Night <davidjantho@gmail.com>
2022-07-08 19:31:08 +0200

    updated service template

--- install.py
+++ install.py
@@ -5,11 +5,14 @@ import subprocess
 try:
     with open("keep-server-awake.service.template", 'r') as f:
         service = f.read(-1)
-    service = service.replace("<file_path>", f'{getcwdb().decode("utf-8")}/main.py')
+    service = service.replace(
+        "<folder_path>", f'{getcwdb().decode("utf-8")}').replace("file_name", "main.py")
     with open("/etc/systemd/system/keep-server-awake.service", "w") as f:
         f.write(service)
     subprocess.call(["sudo", "systemctl", "daemon-reload"])
-    subprocess.call(["sudo", "systemctl", "enable", "keep-server-awake.service"])
-    subprocess.call(["sudo", "systemctl", "start", "keep-server-awake.service"])
+    subprocess.call(["sudo", "systemctl", "enable",
+                    "keep-server-awake.service"])
+    subprocess.call(["sudo", "systemctl", "start",
+                    "keep-server-awake.service"])
 except Exception as ex:
     print("Service creation failed, please try starting this with sudo!")
