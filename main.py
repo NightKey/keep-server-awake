449c2d169e4670e78e5f9e4d6019a7c7f861bb01
Night <davidjantho@gmail.com>
2022-07-15 13:35:45 +0200

    Added additional logging

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ except:
     exit(1)
 
 
+class ConfigException(Exception):
+    pass
+
+
+class DifferentLenghtException(Exception):
+    def __init__(self) -> None:
+        super().__init__("The computer IPs and MACs should have the same length!")
+
+
 logger = Logger("keep-server-awake.log", "/var/log", level="DEBUG",
                 log_to_console=True, storage_life_extender_mode=True, max_logfile_size=500)
 
@@ -26,12 +35,16 @@ class Config:
         self.log_level = log_level
 
     def load(file_path: str) -> 'Config':
-        with open(file_path, "r") as f:
-            config = json.load(f)
-        return Config(config['computers']["ips"], config['computers']["macs"], config["local broadcast address"], int(config["wait between pings in seconds"]), config["log folder"], config["log level"])
+        try:
+            with open(file_path, "r") as f:
+                config = json.load(f)
+            return Config(config['computers']["ips"], config['computers']["macs"], config["local broadcast address"], int(config["wait between pings in seconds"]), config["log folder"], config["log level"])
+        except Exception as ex:
+            raise ConfigException(ex.args)
 
 
 def ping(ip_address: str) -> bool:
+    logger.debug(f"Pinging the following IP: {ip_address}")
     command = ['ping', '-c', '1', ip_address]
     return subprocess.call(command) == 0
 
@@ -61,11 +74,18 @@ def main(install_path: str):
 }""")
         logger.info(
             f'Config file created.\nPlease fill in the config.conf file in "{realpath(install_path)}" with the relevant info.')
-        exit(0)
+        raise ConfigException("No configuration file was present!")
+    logger.debug("Loading configuration")
     config = Config.load(config_path)
+    if len(config.ips) != len(config.macs):
+        logger.error("The computer IPs and MACs should have the same length!")
+        raise DifferentLenghtException
     logger.info(f"Started with {len(config.ips)} server(s) set.")
-    logger.set_level(config.log_level)
-    logger.log_folder = config.log_folder
+    try:
+        logger.set_level(config.log_level)
+        logger.set_folder(config.log_folder)
+    except Exception as ex:
+        raise ConfigException(ex.args)
     while True:
         try:
             for ip, mac in zip(config.ips, config.macs):
